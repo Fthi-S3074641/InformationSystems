1d3fff271266b9e7850eb635892ee85351a4df89
Peter Ullrich <peter.j.ullrich@gmail.com>
2017-11-22 09:46:58 +0100

    Updated measure peformance wrapper

--- decorators.py
+++ decorators.py
@@ -1,10 +1,10 @@
 import time
 
 
-def measure_performance():
-    def wrapper(func):
+def measure_performance(func):
+    def wrapper(*args, **kwargs):
         t1 = time.time()
-        func()
+        func(*args, **kwargs)
         t2 = time.time()
         print(f"Performance of {func.__name__}: {t2 - t1}s")
 
